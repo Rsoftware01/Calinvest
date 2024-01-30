3f8859f2ef0a143b9d0c00ba7e8fc5d8b73d8f5c
Rafael Almeida <rafael.almeida2210@icloud.com>
2024-01-29 23:10:16 -0300

    Atualizações de erro no formulário

--- main.js
+++ main.js
@@ -5,16 +5,20 @@ const form = document.getElementById("investment-form");
 function renderProgression(evt) {
   evt.preventDefault();
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const additionalContribution = Number(
-    document.getElementById("additional-contribution").value
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -27,5 +31,30 @@ function renderProgression(evt) {
   );
   console.log(returnsArray);
 }
+
+function validateInput(evt) {
+  if (evt.target.value === "") {
+    return;
+  }
+
+  const { parentElement } = evt.target;
+  const grandParentElement = evt.target.parentElement.parentElement;
+
+  const inputValue = evt.target.value.replace(",", ".");
+  if (isNaN(inputValue) || Number(inputValue) <= 0) {
+    const errorTextElement = document.createElement("p"); //<p></p>
+    errorTextElement.classList.add("text-red-500"); //<p class =""></p>
+    errorTextElement.classList.add("font-bold"); //<p class =""></p>
+    errorTextElement.innerText = "Insira um valor maior do que zero"; //<p class="text-red-500 font-bold"> Insira um valor maior do que zero</p>
+
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(errorTextElement);
+  }
+}
+
+for (const formElement of form)
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
 
 form.addEventListener("submit", renderProgression);
